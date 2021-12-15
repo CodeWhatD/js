05221f62a72f3d6f2e46de3725720098f503e34d
11427 <1911650840@qq.com>
2021-12-15 21:51:07 +0800

    [ADD]Promise读取多个文件小案例

---
+++ 07-ES6/24-Promise读取多个文件.js
@@ -0,0 +1,26 @@
+const fs = require("fs");
+let p = new Promise((resolve, reject) => {
+    fs.readFile('./01-let.html', (err, data) => {
+        if (err) reject(err)
+        resolve(data.toString())
+    })
+})
+p.then(value => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./02-const.html', (err, data) => {
+            if (err) reject(err)
+            resolve([value, data])
+        })
+    })
+}).then(value => {
+    return new Promise((resolve, reject) => {
+        fs.readFile('./03-解构.html', (err, data) => {
+            if (err) reject(err)
+            console.log()
+            value.push(data)
+            resolve(value)
+        })
+    })
+}).then(value => {
+    console.log(value.toString())
+})
