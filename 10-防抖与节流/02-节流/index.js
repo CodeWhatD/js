db46748f7081913524580b517df753d5fb83b8c3
杜梦越 <myduh@isoftstone.com>
2022-06-02 13:59:39 +0800

    [FIX]节流代码问题修改

--- 10-防抖与节流/02-节流/index.js
+++ 10-防抖与节流/02-节流/index.js
@@ -1,6 +1,6 @@
 const throttle = function (fn, time) {
 	let timeOne = 0;
-	return () => {
+	return function() {
 		let timeTwo = new Date();
 		if (timeTwo - timeOne > time) {
 			let args = arguments;
